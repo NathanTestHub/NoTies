2f718757fd111f9ea69b3e945fc2406f42b14201
Nathan Orawi <orawinathan@gmail.com>
2025-08-24 23:31:40 +1000

    Implement login page component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/chat" element={<Chat/>} />
         <Route path="/profile-update" element={<ProfileUpdate/>} />
       </Routes>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,25 @@
 import React from 'react' 
 import './Login.css'
+import assets from '../../assets/assets'
 
 const Login = () => {
   return (
-    <div>
-      we are on the login page.
+    <div className= 'login'>
+      <img src={assets.logo_big} alt="NoTies Logo" className="logo" />
+      <form className="login-form"> 
+        <h2 className="sign-up-header">Sign Up</h2>
+        <input type="text" placeholder="Username" className="input-field" required/>
+        <input type="email" placeholder="Email" className="input-field" required/>
+        <input type="password" placeholder="Password" className="input-field" required/>
+        <button type="submit" className="login-button">Sign Up</button>
+        <div className="login-term">
+          <input type="checkbox" className="checkbox" />
+          <p>By signing up, you agree to our Terms & Conditions</p>
+        </div>
+        <div className="login-forgot">
+          <p className="login-toggle">Already have an account? <a href="/login">Log In</a>  </p> 
+        </div>
+      </form>
     </div>
   )
 }
