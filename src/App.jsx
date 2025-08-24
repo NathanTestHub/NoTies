65a167a19239853bffca511c96264e82b28ee857
Nathan Orawi <orawinathan@gmail.com>
2025-08-24 21:30:25 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login/Login.jsx'
+import Chat from './pages/Chat/Chat.jsx'
+import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate.jsx'
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/chat" element={<Chat/>} />
+        <Route path="/profile-update" element={<ProfileUpdate/>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
