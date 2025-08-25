6c4b86ad67fbfd02c56b9db9caad7edd788ed1cf
Nathan Orawi <orawinathan@gmail.com>
2025-08-25 22:18:16 +1000

    Implement LeftSidebar of Chat page component and styling

---
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./LeftSidebar.css";
+import assets from "../../assets/assets";
+
+const LeftSidebar = () => {
+  return (
+    <div className="left-sidebar">
+      <div className="left-sidebar-top">
+        <div className="left-sidebar-nav">
+          <img src={assets.logo} className="logo" alt="" />
+          <div className="menu">
+            <img src={assets.menu_icon} alt="" />
+          </div>
+        </div>
+        <div className="left-sidebar-search">
+          <img src={assets.search_icon} alt="" />
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+      <div className="left-sidebar-list">
+        {Array(12)
+          .fill("")
+          .map((item, index) => (
+            <div key={index} className="friends">
+              <img src={assets.profile_img} alt="" />
+              <div>
+                <p>John Doe</p>
+                <span>Hey! How are you?</span>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default LeftSidebar;
