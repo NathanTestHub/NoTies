d4870bd86ae35d1b573f86bbdeb665d0d79fbaf4
Nathan Orawi <orawinathan@gmail.com>
2025-08-25 10:15:14 +1000

    Implement combined login and sign up form with toggle functionality

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,24 +1,33 @@
-import React from 'react' 
+import React, {useState} from 'react' 
 import './Login.css'
 import assets from '../../assets/assets'
 
 const Login = () => {
+
+    const [currentForm, setCurrentForm] = useState("Sign Up"); 
+
   return (
     <div className= 'login'>
       <img src={assets.logo_big} alt="NoTies Logo" className="logo" />
       <form className="login-form"> 
-        <h2 className="sign-up-header">Sign Up</h2>
-        <input type="text" placeholder="Username" className="input-field" required/>
+        <h2 className="sign-up-header">{currentForm}</h2>
+        {currentForm === "Sign Up"?<input type="text" placeholder="Username" className="input-field" required/>: null}
         <input type="email" placeholder="Email" className="input-field" required/>
         <input type="password" placeholder="Password" className="input-field" required/>
-        <button type="submit" className="login-button">Sign Up</button>
+        <button type="submit" className="login-button">{currentForm === "Sign Up"?"Create account":"Login now"}</button>
         <div className="login-term">
           <input type="checkbox" className="checkbox" />
           <p>By signing up, you agree to our Terms & Conditions</p>
         </div>
         <div className="login-forgot">
-          <p className="login-toggle">Already have an account? <a href="/login">Log In</a>  </p> 
-        </div>
+          {
+            currentForm === "Sign Up"
+            ?<p className="login-toggle">Already have an account? <span onClick={()=>setCurrentForm("Login")}>Login here</span> </p> 
+            :<p className="login-toggle">Create an account <span onClick={()=>setCurrentForm("Sign Up")}>Sign Up here</span> </p> 
+        
+          }
+          </div>
+        
       </form>
     </div>
   )
