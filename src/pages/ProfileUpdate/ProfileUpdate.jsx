8c95067e75e72b1c967c7b164a3620f09dc93884
Nathan Orawi <orawinathan@gmail.com>
2025-08-27 16:20:02 +1000

    Implement ProfileUpdate page component and styling

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import './ProfileUpdate.css'
+import React from "react";
+import "./ProfileUpdate.css";
+import assets from "../../assets/assets";
+import { useState } from "react";
 
 const ProfileUpdate = () => {
-  return (
-    <div>
-      we are on the profile update page.
-    </div>
-  )
-}
+  const [image, setImage] = useState(false);
 
-export default ProfileUpdate
+  return (
+    <div className="profile-update">
+      <div className="profile-update-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input
+              onChange={(e) => setImage(e.target.files[0])}
+              type="file"
+              id="avatar"
+              accept=".png, .jpg, jpeg"
+              hidden
+            />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.avatar_icon}
+              alt=""
+            />
+            upload profile picture
+          </label>
+          <input type="text" placeholder="Name" required />
+          <textarea placeholder="Bio" required />
+          <button type="submit">Save</button>
+        </form>
+        <img
+          className="profile-pic"
+          src={image ? URL.createObjectURL(image) : assets.logo_icon}
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProfileUpdate;
