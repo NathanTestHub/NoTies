11d5e79d69db7e88882b8cd1c7cbd933201011a1
Nathan Orawi <orawinathan@gmail.com>
2025-09-14 00:33:53 +1000

    Initialized chat between to users

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -2,9 +2,20 @@ import React, { useContext, useState } from "react";
 import "./LeftSidebar.css";
 import assets from "../../assets/assets";
 import { useNavigate } from "react-router-dom";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import {
+  arrayUnion,
+  collection,
+  doc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { db } from "../../config/firebase";
 import { AppContext } from "../../context/AppContext";
+import { toast } from "react-toastify";
 
 const LeftSidebar = () => {
   const navigate = useNavigate();
@@ -25,11 +36,46 @@ const LeftSidebar = () => {
         } else {
           setUser(null);
         }
-      }
-      else {
+      } else {
         setShowSearch(false);
       }
     } catch (error) {}
+  };
+
+  const addChat = async () => {
+    const messageRef = collection(db, "messages");
+    const chatsRef = collection(db, "chats");
+    try {
+      const newMessageRef = doc(messageRef);
+
+      await setDoc(newMessageRef, {
+        createAt: serverTimestamp(),
+        messages: [],
+      });
+
+      await updateDoc(doc(chatsRef, user.id), {
+        chatsData: arrayUnion({
+          messageId: newMessageRef.id,
+          lastMessage: "",
+          rId: userData.id,
+          updatedAt: Date.now(),
+          messageSeen: true,
+        }),
+      });
+
+      await updateDoc(doc(chatsRef, userData.id), {
+        chatsData: arrayUnion({
+          messageId: newMessageRef.id,
+          lastMessage: "",
+          rId: user.id,
+          updatedAt: Date.now(),
+          messageSeen: true,
+        }),
+      });
+    } catch (error) {
+      toast.error(error.message);
+      console.error(error);
+    }
   };
 
   return (
@@ -52,23 +98,24 @@ const LeftSidebar = () => {
         </div>
       </div>
       <div className="left-sidebar-list">
-        {showSearch && user
-        ? <div className="friends add-user">
-            <img src={user.avatar} alt = ''/>
+        {showSearch && user ? (
+          <div onClick={addChat} className="friends add-user">
+            <img src={user.avatar} alt="" />
             <p>{user.name}</p>
-        </div>
-        :Array(12)
-          .fill("")
-          .map((item, index) => (
-            <div key={index} className="friends">
-              <img src={assets.profile_img} alt="" />
-              <div>
-                <p>John Doe</p>
-                <span>Hey! How are you?</span>
+          </div>
+        ) : (
+          Array(12)
+            .fill("")
+            .map((item, index) => (
+              <div key={index} className="friends">
+                <img src={assets.profile_img} alt="" />
+                <div>
+                  <p>John Doe</p>
+                  <span>Hey! How are you?</span>
+                </div>
               </div>
-            </div>
-          ))
-          }
+            ))
+        )}
       </div>
     </div>
   );
