67e9526fff2c34cb6c8a79fe0cfbe5c7bb847015
Nathan Orawi <orawinathan@gmail.com>
2025-09-16 12:45:59 +1000

    Refactor right-sidebar to reflect user

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,31 +1,48 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./RightSidebar.css";
 import assets from "../../assets/assets";
 import { logout } from "../../config/firebase";
+import { AppContext } from "../../context/AppContext";
 
 const RightSidebar = () => {
-  return (
+  const { chatUser, messages } = useContext(AppContext);
+  const [messageImages, setMessageImages] = useState([]);
+
+  useEffect(() => {
+    let tempVar = [];
+    messages.map((message) => {
+      if (message.image) [tempVar.push(message.image)];
+    });
+    setMessageImages(tempVar)
+  }, [messages]);
+
+  return chatUser ? (
     <div className="right-sidebar">
       <div className="right-sidebar-profile">
-        <img src={assets.profile_img} alt="" />
+        <img src={chatUser.userData.avatar} alt="" />
         <h3>
-          John Doe
+          {chatUser.userData.name}
           <img src={assets.green_dot} className="dot" alt="" />
         </h3>
-        <p>Hey, there i'm using NoTies.</p>
+        <p>{chatUser.userData.bio}</p>
       </div>
       <hr />
       <div className="right-sidebar-media">
         <p>Media</p>
         <div>
-          <img src={assets.pic1} alt="" />
+          {messageImages.map((url, index)=>(<img onClick={()=>window.open(url)} key={index} src={url} alt=''/>))}
+          {/* <img src={assets.pic1} alt="" />
           <img src={assets.pic2} alt="" />
           <img src={assets.pic3} alt="" />
           <img src={assets.pic4} alt="" />
           <img src={assets.pic1} alt="" />
-          <img src={assets.pic2} alt="" />
+          <img src={assets.pic2} alt="" /> */}
         </div>
       </div>
+      <button onClick={() => logout()}>Logout</button>
+    </div>
+  ) : (
+    <div className="right-sidebar">
       <button onClick={() => logout()}>Logout</button>
     </div>
   );
